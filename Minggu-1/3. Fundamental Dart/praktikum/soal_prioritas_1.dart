18cbf364bb6c64f32a2caa754a6611690d303d67
aryaptradji <aryaptradji@gmail.com>
2023-08-25 12:04:53 +0700

    completed soal prioritas 1. tugas fundamental dart. minggu-1

---
+++ Minggu-1/3. Fundamental Dart/praktikum/soal_prioritas_1.dart
@@ -0,0 +1,14 @@
+void main(List<String> args) {
+  //# Rumus Keliling dan Luas Persegi
+  // sisi persegi
+  int s = 10;
+
+  // rumus keliling persegi
+  final perimeter = 4 * s;
+  // rumus luas persegi
+  final area = s * s;
+
+  // hasil
+  print(perimeter);
+  print(area);
+}
