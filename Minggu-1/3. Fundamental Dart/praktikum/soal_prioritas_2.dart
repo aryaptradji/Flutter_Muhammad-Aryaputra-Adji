e7a06998967fe35b67bb375b52edd7d055346d20
aryaptradji <aryaptradji@gmail.com>
2023-08-25 20:50:27 +0700

    completed soal prioritas 2. tugas fundamental dart. minggu-1

--- Minggu-1/3. Fundamental Dart/praktikum/soal_prioritas_2.dart
+++ Minggu-1/3. Fundamental Dart/praktikum/soal_prioritas_2.dart
@@ -1,18 +1,28 @@
-void main(List<String> args) {
+void main() {
   //* Soal [1]
   print("Soal [1]");
-  // membuat 3 variabel string
+  // Membuat 3 variabel string
   String hello = "Hello";
   String greet = "Good Morning";
   String nama = "Adji";
 
-  // menggabungkan 3 variabel
+  // Menggabungkan 3 variabel
   String hasil = "$hello, $greet, Perkenalkan nama saya $nama";
 
-  // Mmnampilkan hasil
+  // Menampilkan hasil
   print(hasil);
   print("\n");
 
   //? Soal [2]
   print("Soal [2]");
+  // Menentukan nilai tinggi tabung
+  double tinggi = 10.0;
+  // Menentukan nilai jari-jari tabung
+  double jariJari = 5.0;
+
+  // Menghitung volume tabung
+  double volume = 3.14 * jariJari * jariJari * tinggi;
+
+  // Menampilkan hasil volume tabung
+  print("Volume tabung adalah: $volume");
 }
