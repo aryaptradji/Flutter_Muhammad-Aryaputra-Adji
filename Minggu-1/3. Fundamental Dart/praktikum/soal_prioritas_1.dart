4f6e10077d3ff8c027706fe63d4168a631c6a60e
aryaptradji <aryaptradji@gmail.com>
2023-08-25 18:32:03 +0700

    update soal prioritas 1. tugas fundamental dart. minggu-1

--- Minggu-1/3. Fundamental Dart/praktikum/soal_prioritas_1.dart
+++ Minggu-1/3. Fundamental Dart/praktikum/soal_prioritas_1.dart
@@ -1,13 +1,46 @@
+import 'dart:math';
+
 void main(List<String> args) {
-  // sisi persegi
+  //* Soal [1]
+  print("// Soal [1]");
+  // Menentukan nilai sisi persegi
   int s = 10;
 
-  // rumus keliling persegi
-  final perimeter = 4 * s;
-  // rumus luas persegi
-  final area = s * s;
+  // Menghitung keliling persegi
+  final keliling_persegi = 4 * s;
+  // Menghitung luas persegi
+  final luas_persegi = s * s;
 
-  // hasil
-  print(perimeter);
-  print(area);
+  // Menampilkan hasil keliling dan luas persegi
+  print("Keliling Persegi : $keliling_persegi");
+  print("Luas Persegi : $luas_persegi");
+
+  // Menentukan nilai panjang dan lebar persegi panjang
+  double panjang = 5.0;
+  double lebar = 3.0;
+
+  // Menghitung keliling persegi panjang
+  double keliling_persegipanjang = 2 * (panjang + lebar);
+
+  // Menghitung luas persegi panjang
+  double luas_persegipanjang = panjang * lebar;
+
+  // Menampilkan hasil keliling dan luas persegi panjang
+  print('Keliling Persegi Panjang: $keliling_persegipanjang');
+  print('Luas Persegi Panjang: $luas_persegipanjang\n');
+
+  //? Soal 2
+  print("// Soal [2]");
+  // Menentukan nilai jari-jari lingkaran
+  double jariJari_lingkaran = 5.0;
+
+  // Menghitung keliling lingkaran
+  double keliling_lingkaran = 2 * pi * jariJari_lingkaran;
+
+  // Menghitung luas lingkaran
+  double luas_lingkaran = pi * pow(jariJari_lingkaran, 2);
+
+  // Menampilkan hasil keliling dan luas lingkaran
+  print('Keliling Lingkaran: $keliling_lingkaran');
+  print('Luas Lingkaran: $luas_lingkaran');
 }
---
+++ Minggu-1/3. Fundamental Dart/praktikum/soal_prioritas_2.dart
@@ -0,0 +1,12 @@
+void main(List<String> args) {
+  // membuat 3 variabel string
+  String hello = "Hello";
+  String greet = "Good Morning";
+  String nama = "Adji";
+
+  // menggabungkan 3 variabel
+  String hasil = "$hello, $greet, Perkenalkan nama saya $nama";
+
+  // Mmnampilkan hasil
+  print(hasil);
+}
