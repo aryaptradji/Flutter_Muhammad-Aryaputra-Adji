62f20f15cc4352093c65adb64ae7f63746d4756a
aryaptradji <aryaptradji@gmail.com>
2023-08-28 17:05:10 +0700

    completed soal prioritas 1. tugas branching-looping-function. minggu-2

---
+++ Minggu-2/1. Branching-Looping-Function/praktikum/soal_prioritas_1.dart
@@ -0,0 +1,29 @@
+void main() {
+//* Tugas Percabangan (Branching)
+  print("// Tugas Percabangan (Branch)");
+  int nilai1 = 85;
+  int nilai2 = 50;
+  int nilai3 = 25;
+
+  String checkNilai(int nilai) {
+    if (nilai > 70) {
+      return "Nilai A";
+    } else if (nilai > 40 && nilai <= 70) {
+      return "Nilai B";
+    } else if (nilai > 0 && nilai <= 40) {
+      return "Nilai C";
+    } else {
+      return "";
+    }
+  }
+
+  print("$nilai1: ${checkNilai(nilai1)}");
+  print("$nilai2: ${checkNilai(nilai2)}");
+  print("$nilai3: ${checkNilai(nilai3)}");
+
+  //? Tugas Perulangan (Looping)
+  print("\n// Tugas Perulangan (Looping)");
+  for (var i = 1; i <= 10; i++) {
+    print(i);
+  }
+}
