ef52df1a2b622f16386401d3c97d6f54b4ab041d
aryaptradji <aryaptradji@gmail.com>
2023-08-29 16:49:31 +0700

    completed soal eksplorasi. tugas branching-looping-function. minggu-2

--- Minggu-2/1. Branching-Looping-Function/praktikum/soal_eksplorasi.dart
+++ Minggu-2/1. Branching-Looping-Function/praktikum/soal_eksplorasi.dart
@@ -1,34 +1,57 @@
-/*
-Program to Check Prime Numbers
-
-This program provides a function to check whether a number is a prime number or not.
-
-*/
-
 import 'dart:math';
-
-bool isPrime(int number) {
-  // Check if the number is less than 2
-  if (number < 2) {
-    return false;
-  }
-
-  // Check if the number is divisible by any number from 2 to sqrt(number)
-  for (int i = 2; i <= sqrt(number); i++) {
-    if (number % i == 0) {
-      return false;
-    }
-  }
-
-  return true;
-}
+import 'dart:io';
 
 void main() {
-  // Example usage
-  int number = 23;
-  if (isPrime(number)) {
-    print('prime number');
+  //^ Soal [1]
+  print("// Soal [1]");
+  // Membuat fungsi untuk mengecek bilangan prima atau bukan
+  bool isPrime(int bilangan) {
+    // Mengecek bilangan prima yang tidak boleh kurang dari 2
+    if (bilangan < 2) {
+      return false;
+    }
+
+    // Mengecek apakah bilangan tersebut habis dibagi bilangan apapun dari 2 hingga akar kuadrat(bilangan)
+    for (int i = 2; i <= sqrt(bilangan); i++) {
+      if (bilangan % i == 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  // Menginisiasi nilai bilangan yang akan dicek
+  int bilangan1 = 23;
+  int bilangan2 = 12;
+  // Menampilkan hasil pengecekan bilangan prima
+  if (isPrime(bilangan1)) {
+    print('$bilangan1 = bilangan prima');
   } else {
-    print('not a prime number');
+    print('$bilangan1 = bukan bilangan prima');
+  }
+  if (isPrime(bilangan2)) {
+    print('$bilangan2 = bilangan prima');
+  } else {
+    print('$bilangan2 = bukan bilangan prima');
+  }
+
+  //$ Soal [2]
+  print("\n// Soal [2]");
+  // Menentukan nilai maksimal dari perkalian
+  int input = 9;
+  int maxValue = input * input;
+
+  // Menghitung panjang digit terpanjang untuk penataan tabel
+  int maxDigitCount = maxValue.toString().length;
+
+  // Membuat tabel perkalian
+  for (int i = 1; i <= input; i++) {
+    for (int j = 1; j <= input; j++) {
+      int product = i * j;
+      String formattedProduct = product.toString().padLeft(maxDigitCount);
+
+      stdout.write(formattedProduct + " ");
+    }
+    print("\n");
   }
 }
