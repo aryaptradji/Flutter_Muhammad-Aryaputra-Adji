05e345bc7c04038e929eb6bdb8b17bb36f78e861
aryaptradji <aryaptradji@gmail.com>
2023-09-03 16:09:45 +0700

    updated soal prioritas 2 and added soal eksplorasi. tugas advance function-async-await-collection. minggu-2

--- Minggu-2/2. Advance Function-Async-Await-Collection/praktikum/soal_prioritas_2.dart
+++ Minggu-2/2. Advance Function-Async-Await-Collection/praktikum/soal_prioritas_2.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 void main() {
+  //* Soal 1
+  print('// Soal 1');
   List<List<int>> dataList = [
     [1, 2],
     [3, 4],
@@ -12,4 +16,36 @@ void main() {
   }
 
   print(dataMap);
+
+  //$ Soal 2
+  print('\n// Soal 2');
+  List<int> nilai = [7, 5, 3, 5, 2, 1];
+
+  double total = 0;
+  for (int i = 0; i < nilai.length; i++) {
+    total += nilai[i];
+  }
+
+  double rataRata = total / nilai.length;
+  int rataRataBulat = rataRata.ceil();
+
+  print(rataRataBulat);
+
+  //# Soal 3
+  print('\n// Soal 3');
+  int input = 5;
+
+  factorialAsync(input).then((result) {
+    print(result);
+  }).catchError((error) {
+    print('Error: $error');
+  });
+}
+
+Future<int> factorialAsync(int n) async {
+  if (n == 0) {
+    return 1;
+  } else {
+    return n * await factorialAsync(n - 1);
+  }
 }
